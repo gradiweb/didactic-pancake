7c8b54940578270bf59b3ad25af2616d9455d06e
entornoInteractivo <cristianvelasco1529@gmail.com>
2022-06-21 21:19:26 -0500

    styles filters modal

--- src/templates/collection.js
+++ src/templates/collection.js
@@ -1,1 +1,18 @@
 import "../scss/collection.scss";
+import { toggleDataActive } from "../utils/toggle-dataset";
+
+toggleDataActive(
+  '#filter-open',
+  '#filter',
+  {
+    overlay: true
+  }
+)
+
+toggleDataActive(
+  '.filter-form_name svg',
+  '#filter',
+  {
+    overlay: true
+  }
+)
