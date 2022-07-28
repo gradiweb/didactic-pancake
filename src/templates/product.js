6bab3f131e5e905d0208321dceac279907cddc55
entornoInteractivo <cristianvelasco1529@gmail.com>
2022-07-28 18:04:20 -0500

    udpate grid and depure

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,6 +1,6 @@
 import "../scss/product.scss";
 import { mountSlider } from "../components/product-slider";
 import { $Q } from "../utils/query-selector";
-import '../components/recomendaciones-api'
+import '../components/upsell-api'
 
 mountSlider($Q('.swiper.main-product__slider-principal'));
