488c67b436e27bb49e5230e2144c2eba97531338
crisdeve <cristian@gradiweb.com>
2022-08-09 21:05:53 -0500

    fixes lt reuse snippet

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,10 +6,8 @@ import { variantOnChange } from "../components/variants-product";
 import { openAccordion } from "../utils/accordion";
 import { openDropDown } from "../components/header";
 import { toggleDataActive } from "../utils/toggle-dataset";
-import { initReviewsSlider } from "../components/reviews-slider";
 
 variantOnChange(".variants");
 openAccordion();
 openDropDown();
 toggleDataActive("#burger-nav-js", "#nav-list-js");
-initReviewsSlider();
