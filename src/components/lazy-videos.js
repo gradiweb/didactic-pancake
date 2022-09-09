20c98de0a722bf9869bc55c22b0eac2cb2bb4d0c
Rafael Andres Granados <rafael.granados@gradiweb.com>
2022-09-09 09:42:14 -0500

    adding video media

---
+++ src/components/lazy-videos.js
@@ -0,0 +1,29 @@
+import { $Q, $Qll } from "../utils/query-selector";
+
+export const lazyVideo = () => {
+  let lazyVideos = [].slice.call($Qll("video.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyVideoObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((video) => {
+        if (video.isIntersecting) {
+          for (let source in video.target.children) {
+            let videoSource = video.target.children[source];
+            if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+              videoSource.src = videoSource.dataset.src;
+            }
+          }
+
+          video.target.load();
+          video.target.classList.remove("lazy");
+          lazyVideoObserver.unobserve(video.target);
+        }
+      });
+    });
+
+    lazyVideos.forEach((lazyVideo) => {
+      lazyVideoObserver.observe(lazyVideo);
+    });
+  }
+
+}
--- src/templates/index.js
+++ src/templates/index.js
@@ -6,8 +6,10 @@ import { variantOnChange } from "../components/variants-product";
 import { openAccordion } from "../utils/accordion";
 import { openDropDown } from "../components/header";
 import { toggleDataActive } from "../utils/toggle-dataset";
+import { lazyVideo } from '../components/lazy-videos'
 
 variantOnChange(".variants");
 openAccordion();
 openDropDown();
 toggleDataActive("#burger-nav-js", "#nav-list-js");
+lazyVideo();
