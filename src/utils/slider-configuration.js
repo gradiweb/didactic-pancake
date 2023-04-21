7c33c48e3f49de81088b0202b1671ca9fe0a1d8a
EdinsonF <figueroaedinson@gmail.com>
2023-04-21 09:05:08 -0500

    slider product dinamic

--- src/utils/slider-configuration.js
+++ src/utils/slider-configuration.js
@@ -23,7 +23,8 @@ export const thumbsConfig = (slidesPerView, vertical) => {
   return config;
 }
 
-export const principalConfig = (thumbnails) => ({
+export const principalConfig = (thumbnails, vertical) => {
+  const config = {
     modules: [Navigation, Thumbs],
     spaceBetween: 10,
     thumbs: {
@@ -33,4 +34,9 @@ export const principalConfig = (thumbnails) => ({
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  })
+  }
+  // eslint-disable-next-line no-unused-expressions
+  vertical && (config.direction = "vertical");
+
+  return config;
+}
